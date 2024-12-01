40bd23105e7ae2db67add789f8ae0e440e158de1
vstack2 <160262822+vstack2@users.noreply.github.com>
2024-12-01 11:17:02 -0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var username = process.env.username;
 var password = process.env.password;
 var shared_secret = process.env.shared;
 
-var games = [730, 440, 570];  // Enter here AppIDs of the needed games
+var games = [730, 2923300, 1326570, 670290];  // Enter here AppIDs of the needed games
 var status = 1;  // 1 - online, 7 - invisible
 
 
